c4a7e9a8db37e7a7905a29143b7f3e256a6b80ab
Gastón Caminiti <gastoncaminiti@outlook.com>
2021-05-25 20:40:00 -0300

    05 Swipe y animación marchitar
    Code: Botones Touch
    Code: Simulación Swipe UP
    Asset: Animación marchitar y recoger

--- project/scripts/flower.gd
+++ project/scripts/flower.gd
@@ -5,18 +5,30 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var rng = RandomNumberGenerator.new()
+var selected
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
-	$TimerGrow.set_wait_time(rng.randf_range(0.5, 3.0))
-	$TimerGrow.start()
-
+	live()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
 func _on_TimerGrow_timeout():
 	$AnimationPlayer.play("GROW")
+
+func _on_TouchButtonTop_pressed():
+	if selected:
+		#queue_free()
+		live()
+
+func _on_TouchButtonBottom_pressed():
+	selected = true
+
+func live():
+	var random = rng.randf_range(0.5, 2.0)
+	$Sprite.frame = 0
+	$TimerGrow.set_wait_time(random)
+	$TimerGrow.start()
+	selected = false
